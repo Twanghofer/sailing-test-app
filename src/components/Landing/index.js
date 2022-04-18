624c1a1f23f9846e0e7639cfa42a1b5f32c377b8
twanghofer <tobias.w01@gmx.at>
2022-04-18 13:52:12 +0200

    adjust header and landing style

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.css";
 
 import SailingShip from "../../assets/ship.jpg";
+import Compass from "../../assets/compass.svg";
 
 function Landing() {
   return (
@@ -13,7 +14,12 @@ function Landing() {
           sollicitudin, lorem
         </h2>
       </div>
-      <img src={SailingShip} alt="Sailing Ship" />
+      <img
+        className="landing-background"
+        src={SailingShip}
+        alt="Sailing Ship"
+      />
+      <img className="compass" src={Compass} alt="Compass" />
     </section>
   );
 }
